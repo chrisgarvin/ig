9245cbb87a69ffea0a246a1326ffd79ac3b8a786
kyleconkright <kyleconkright@gmail.com>
2015-08-22 15:12:20 -0700

    login button

--- client/ig.js
+++ client/ig.js
@@ -1,6 +1,16 @@
-Meteor.loginWithInstagram(function (err, res) {
-        if (err !== undefined)
-          console.log('success ' + res)
-        else
-          console.log('login failed ' + err)
-    });
+Template.home.events({
+	'click .login': function(){
+		Meteor.loginWithInstagram(function (err, res) {
+		    if (err !== undefined)
+		      console.log('success ' + res)
+		    else
+		      console.log('login failed ' + err)
+		});
+	},
+
+	'click .logout': function(){
+		Meteor.logout();
+	}
+});
+
+
