c6a89a318fba2136dd2e3ea9223eb288dbe98fe9
Chris Garvin <chris.garvin@me.com>
2015-08-22 17:43:01 -0700

    did a lot of stuff that doesnt seem to work, kyle fix it or just delete all of this project

--- client/ig.js
+++ client/ig.js
@@ -14,3 +14,19 @@ Template.home.events({
 		Meteor.logout();
 	}
 });
+
+var getNewPhotos = function () {
+
+	var currentUser = Meteor.users.findOne({_id: Meteor.userId()});
+	var accessToken = currentUser.services.instagram.accessToken;
+	console.log(accessToken)
+	$.getJSON('https://api.instagram.com/v1/tags/' + currentUser.services.instagram.username + '/media/recent?access_token=' + accessToken + '', function(data){
+	for(var i = 0; i < data.data.length; i++){
+		$(".photos").append("<img src='" + data.data[i].images.standard_resolution.url + "'>")
+	};
+	})
+};
+
+$(document).ready(function(){
+	getNewPhotos();
+})
